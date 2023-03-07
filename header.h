2b1f9c604849d8711a6fc9542b9a67553200bf8c
wasdqaz <126933505+wasdqaz@users.noreply.github.com>
2023-03-07 21:22:22 +0700

    test

--- header.h
+++ header.h
@@ -4,29 +4,26 @@
 #include<iostream>
 using namespace std;
 
-//quy tac dat ten bien: viet hoa chu dau moi tu vd HocSinh, Sinh, ....
-//so thap phan de kieu float
 
 struct Account{
     string UserName, Password;
-
     //Type true = giao vien, false = hoc sinh
     //status true la log in con false la log out
     bool Status,Type;
 };
 
 struct Student{
-    Account Acc;
     int No;
     string Name, Id, Class, Course;
     float Gpa, TotalMark, FinalMark, MidtermMark, OtherMark;//yeu cau 20
-    
+    Student *Next, *Prev=nullptr;//pre luc nao co tg thi uppdate
 };
 
 struct Class
 {       
     
 };
+
 
 
 #endif
