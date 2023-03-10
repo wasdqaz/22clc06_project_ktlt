0e6217f1f77f71ec3a860c187d2c6c90cc7c4224
wasdqaz <126933505+wasdqaz@users.noreply.github.com>
2023-03-10 14:37:47 +0700

    hàm đổi mật khẩu

---
+++ header.cpp
@@ -0,0 +1,34 @@
+#include "header.h"
+bool checkPassword(string a){
+    for (int i = 0; i < a.size(); i++)
+        if(a[i]==' ')return false;
+    return true;
+}
+void changePassword(Account &a){
+    string tmp;
+    cout<<"Enter old password: ";
+    getline(cin, tmp);
+    while(tmp!=a.Password){
+        cout<<"Invalid password!!! Please re-enter.\n";
+        getline(cin, tmp);
+    }
+    cout<<"Enter new password: ";
+    cin.ignore();
+    getline(cin, tmp);
+    while(checkPassword(tmp)==0){
+        cout<<"Invalid password!!! Please re-enter.\n";
+        cout<<"The password mustn't include the character " ".\n";
+        cout<<"Enter new password: ";
+        getline(cin, tmp);
+    }
+    a.Password=tmp;
+    cout<<"Change password successfully!!!";
+}
+
+int main(){
+    Account a;
+    a.UserName="phamanhkhoi";
+    a.Password="qweqwe";
+    changePassword(a);
+    return 0;
+}
