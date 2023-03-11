42d44b62e8765e70e7a47261d2f94debcf959d61
EvDDD <126933715+EvDDD@users.noreply.github.com>
2023-03-11 23:11:12 +0700

    Update header.h

--- header.h
+++ header.h
@@ -1,54 +1,55 @@
-#ifndef _HEADER_H_
-#define _HEADER_H_
+    #ifndef _HEADER_H_
+    #define _HEADER_H_
 
-#include<iostream>
-#include<fstream>
+    #include<iostream>
+    #include<fstream>
 
-using namespace std;
+    using namespace std;
 
 
-struct Account{
-    string UserName, Password;
-    bool Status,Type;
-    Account *next;
-};
+    struct Account{
+        string UserName, Password;
+        bool Status,Type;
+        Account *next;
+    };
 
-struct Course;
+    struct Course;
 
-struct Student{
-    int No;
-    string F_Name, L_Name, Id, Class; //da bo cai Course va them Ho Ten
-    float Gpa, TotalMark, FinalMark, MidtermMark, OtherMark;
-    Course *CoursesOfStudentHead; //moi update them
-    Student *Next, *Prev=nullptr;
-};
+    struct Student{
+        int No;
+        string F_Name, L_Name, Id, Class, DateOfBirth, SocialId; //da bo cai Course, them Ho Ten, ngay sinh va CCCD
+        float Gpa, TotalMark, FinalMark, MidtermMark, OtherMark;
+        Course *CoursesOfStudentHead; //moi update them
+        Student *Next, *Prev=nullptr;
+        bool Male; // them gioi tinh
+    };
 
-struct Course
-{
-    string CourseId, CourseName, ClassName, TeacherName, Session, DayOfWeek;
-    int NumberOfCredits, MaxStudent = 50;
-    Student *CourseStudent; // moi update them
-    Course *Next, *Prev = nullptr;
-};
+    struct Course
+    {
+        string CourseId, CourseName, ClassName, TeacherName, Session, DayOfWeek;
+        int NumberOfCredits, MaxStudent = 50;
+        Student *CourseStudent; // moi update them
+        Course *Next, *Prev = nullptr;
+    };
 
-struct Class{
-    Student *StudentHead;
-    Class *next;
-};
+    struct Class{
+        Student *StudentHead;
+        Class *next;
+    };
 
-struct Semester{
-    Course *SemesterHead;
-    Semester *next;
-};
+    struct Semester{
+        Course *SemesterHead;
+        Semester *next;
+    };
 
-struct SchoolYear{
-    string BeginYear, EndYear;
-    Class *ClassHead;
-    Semester *S1, *S2, *S3;
-};
-void changePassword(Account &a);
-bool checkPassword(string a);
-void CreateSchoolYears(SchoolYear Year);
-void CreateClass(Class a);
+    struct SchoolYear{
+        string BeginYear, EndYear;
+        Class *ClassHead;
+        Semester *S1, *S2, *S3;
+    };
+    void changePassword(Account &a);
+    bool checkPassword(string a);
+    void CreateSchoolYears(SchoolYear Year);
+    void CreateClass(Class a);
 
-#endif
+    #endif
