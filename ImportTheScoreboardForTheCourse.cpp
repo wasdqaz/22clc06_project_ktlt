66aaa3ba5b3056576b14448763139511691535e9
DangTrieuKha <126672405+DangTrieuKha@users.noreply.github.com>
2023-04-11 15:58:51 +0700

    Update ImportTheScoreboardForTheCourse.cpp
    Update

--- ImportTheScoreboardForTheCourse.cpp
+++ ImportTheScoreboardForTheCourse.cpp
@@ -1,43 +1,55 @@
 #include "header.h"
-void ImportScoreboardFromFileUser (SchoolYear* headOfyear) 
+
+
+void ImportScoreboardFromFileUser(SchoolYear* headOfyear)
 {
     string idCourse;
-    cout<<"Please enter id course:\n";
-    cin>>idCourse;
+    cout << "Please enter id course:\n";
+    cin >> idCourse;
     string fileName;
-    fileName = "Input_mark_"+ idCourse;
+    fileName = "Input_mark_" + idCourse;
 
-     std::string parentFolderName = "Data"; // replace with the name of the parent folder
-std::string subFolderName = "User_Input"; // replace with the name of the subfolder containing the file
+    std::string parentFolderName = "Data"; // replace with the name of the parent folder
+    std::string subFolderName = "User_Input"; // replace with the name of the subfolder containing the file
 
+    std::string filePath;
 
-std::string filePath;
-
-// Search for the parent folder
-for (const auto& entry : fs::directory_iterator(".")) {
-    if (fs::is_directory(entry.status()) && entry.path().filename() == parentFolderName) {
-        // Search for the subfolder
-        for (const auto& subentry : fs::directory_iterator(entry.path())) {
-            if (fs::is_directory(subentry.status()) && subentry.path().filename() == subFolderName) {
-                // Search for the file
-                for (const auto& file : fs::directory_iterator(subentry.path())) {
-                    if (fs::is_regular_file(file.status()) && file.path().filename() == fileName) {
-                        filePath = file.path().string();
-                        break;
-                    }
-                }
-                break;
-            }
-        }
-        break;
+    // Search for the parent folder
+    _finddata_t parentFolder, subFolder;
+    intptr_t parentHandle, subHandle;
+    parentHandle = _findfirst(parentFolderName.c_str(), &parentFolder);
+    if (parentHandle == -1) {
+        std::cerr << "Parent folder not found!" << std::endl;
+        return;
     }
-}
 
-// Check if the file was found
-if (filePath.empty()) {
-    std::cerr << "File not found!" << std::endl;
-    return;
-}
-InputMarkForStudent(headOfyear,filePath);
+    // Search for the subfolder
+    std::string subFolderPath = parentFolderName + "\\" + subFolderName;
+    subHandle = _findfirst(subFolderPath.c_str(), &subFolder);
+    if (subHandle == -1) {
+        std::cerr << "Subfolder not found!" << std::endl;
+        _findclose(parentHandle);
+        return;
+    }
 
+    // Search for the file
+    std::string searchPattern = subFolderPath + "\\" + fileName + ".*";
+    _finddata_t file;
+    intptr_t handle = _findfirst(searchPattern.c_str(), &file);
+    if (handle == -1) {
+        std::cerr << "File not found!" << std::endl;
+        _findclose(subHandle);
+        _findclose(parentHandle);
+        return;
+    }
+
+    filePath = file.name;
+
+    // Process the file
+    InputMarkForStudent(headOfyear, filePath);
+
+    // Release resources
+    _findclose(handle);
+    _findclose(subHandle);
+    _findclose(parentHandle);
 }
