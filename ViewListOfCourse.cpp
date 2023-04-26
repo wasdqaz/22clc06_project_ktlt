d5fd39727ea9abb7dc009283cb94b80b03e32778
EvDDD <maiducduy1805@gmail.com>
2023-04-26 14:56:18 +0700

    Update ViewListOfCourse.cpp

--- ViewListOfCourse.cpp
+++ ViewListOfCourse.cpp
@@ -8,20 +8,20 @@ void ViewListOfCourse(Course *head)
         return;
     }
     Course *cur = head;
-    cout << "----------------------------------------------------------------------------------------\n";
-    cout << left << setw(20) << "Course Id"
-         << left << setw(30) << "course name"
-         << left << setw(20) << "Class name"
-         << left << setw(20) << "Teacher name";
+    cout << "+---------------------+---------------------------------------------------------------------------+\n";
+    cout << setw(2) << left << "|" << left << setw(20) << "Course ID"
+         << setw(2) << left << "|" << left << setw(30) << "Course name"
+         << setw(2) << left << "|" << left << setw(20) << "Class name"
+         << setw(2) << left << "|" << left << setw(20) << "Teacher name" << "|";
     cout << endl;
-    cout << "----------------------------------------------------------------------------------------\n";
+    cout << "+---------------------+---------------------------------------------------------------------------+\n";
     while (cur)
     {
-        cout << left << setw(20) << cur->CourseId << ""
-             << left << setw(30) << cur->CourseName << ""
-             << left << setw(20) << cur->ClassName << ""
-             << left << setw(20) << cur->TeacherName << "\n";
+        cout << setw(2) << left << "|" << left << setw(20) << cur->CourseId << setw(2) << left << "|"
+             << left << setw(30) << cur->CourseName << setw(2) << left << "|"
+             << left << setw(20) << cur->ClassName << setw(2) << left << "|"
+             << left << setw(20) << cur->TeacherName << "|" << "\n";
         cur = cur->Next;
     }
-    cout << "----------------------------------------------------------------------------------------\n";
+    cout << "+---------------------+---------------------------------------------------------------------------+\n";
 }
