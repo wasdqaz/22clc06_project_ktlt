a6480fe55d25b9c55cd9d3d934c34205f7e7a060
DangTrieuKha <126672405+DangTrieuKha@users.noreply.github.com>
2023-05-13 10:53:05 +0700

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -28,142 +28,142 @@
 //     AccessFileMark(directoryPath, Year_head);
 //     readStaffInfo(Staff_info_head);
 
-    //-------------------------------------login--------------------------------
-    cout << "You are student or staff.\n";
-    cout << "1. Staff\n";
-    cout << "2. Student\n";
-    cin >> Role;
-    string clss;
-    if (Role == 1)
-    {
-        User = Login(Staff_head);
-    }
-    else
-    {
-        User = Login(Student_head);
-        clss = User->Class;
-    }
-    //---------------------------------------------------------------------------------
-    curYear = Year_head;
-    while (curYear->NextYear != nullptr)
-        curYear = curYear->NextYear;
-    Semester_head = defaultSemester(curYear);
-    //CalGpa(Year_head->ClassHead);
-    //calGPASemester(Year_head->ClassHead, Semester_head);
-    //---------------------------------------------------------------------------------
-    while (true)
-    {
-        if (User->Role == 1)
-        {
-            cout << "-----------------------------MENU-------------------------------------\n";
-            cout << "|0. Change the password.                                             |\n";
-            cout << "|1. Create school year.                                              |\n";
-            cout << "|1. Add year.                                                        |\n";
-            cout << "|2. Create class.                                                    |\n";
-            cout << "|3. Add student.                                                     |\n";
-            cout << "|4. Create semester.                                                 |\n";
-            cout << "|5. Add course.                                                      |\n";
-            cout << "|6. Upload file student list for course.                             |\n";
-            cout << "|7. Update course information.                                       |\n";
-            cout << "|8. Add a student to the course.                                     |\n";
-            cout << "|9. Remove a student from the course.                                |\n";
-            cout << "|10. Delete course.                                                  |\n";
-            cout << "|11. View a list of classes.                                         |\n";
-            cout << "|12. View a list of students in a class.                             |\n";
-            cout << "|13. View a list of courses.                                         |\n";
-            cout << "|14. View a list of students in a course.                            |\n";
-            cout << "|15. Export a list of students in a course to a CSV file.            |\n";
-            cout << "|16. Import the scoreboard of a course.                              |\n";
-            cout << "|17. Update a student's result.                                      |\n";
-            cout << "|18. View the scoreboard of a class.                                 |\n";
-            cout << "|19. View the scoreboard of a course.                                |\n";
-            cout << "|20. View profile.                                                   |\n";
-            cout << "|21. Upload file student for class                                    |\n";
-            cout << "|22. Logout.                                                         |\n";
-            cout << "------------------------------END-------------------------------------\n";
+//     //-------------------------------------login--------------------------------
+//     cout << "You are student or staff.\n";
+//     cout << "1. Staff\n";
+//     cout << "2. Student\n";
+//     cin >> Role;
+//     string clss;
+//     if (Role == 1)
+//     {
+//         User = Login(Staff_head);
+//     }
+//     else
+//     {
+//         User = Login(Student_head);
+//         clss = User->Class;
+//     }
+//     //---------------------------------------------------------------------------------
+//     curYear = Year_head;
+//     while (curYear->NextYear != nullptr)
+//         curYear = curYear->NextYear;
+//     Semester_head = defaultSemester(curYear);
+//     //CalGpa(Year_head->ClassHead);
+//     //calGPASemester(Year_head->ClassHead, Semester_head);
+//     //---------------------------------------------------------------------------------
+//     while (true)
+//     {
+//         if (User->Role == 1)
+//         {
+//             cout << "-----------------------------MENU-------------------------------------\n";
+//             cout << "|0. Change the password.                                             |\n";
+//             cout << "|1. Create school year.                                              |\n";
+//             cout << "|1. Add year.                                                        |\n";
+//             cout << "|2. Create class.                                                    |\n";
+//             cout << "|3. Add student.                                                     |\n";
+//             cout << "|4. Create semester.                                                 |\n";
+//             cout << "|5. Add course.                                                      |\n";
+//             cout << "|6. Upload file student list for course.                             |\n";
+//             cout << "|7. Update course information.                                       |\n";
+//             cout << "|8. Add a student to the course.                                     |\n";
+//             cout << "|9. Remove a student from the course.                                |\n";
+//             cout << "|10. Delete course.                                                  |\n";
+//             cout << "|11. View a list of classes.                                         |\n";
+//             cout << "|12. View a list of students in a class.                             |\n";
+//             cout << "|13. View a list of courses.                                         |\n";
+//             cout << "|14. View a list of students in a course.                            |\n";
+//             cout << "|15. Export a list of students in a course to a CSV file.            |\n";
+//             cout << "|16. Import the scoreboard of a course.                              |\n";
+//             cout << "|17. Update a student's result.                                      |\n";
+//             cout << "|18. View the scoreboard of a class.                                 |\n";
+//             cout << "|19. View the scoreboard of a course.                                |\n";
+//             cout << "|20. View profile.                                                   |\n";
+//             cout << "|21. Upload file student for class                                    |\n";
+//             cout << "|22. Logout.                                                         |\n";
+//             cout << "------------------------------END-------------------------------------\n";
 
-            int Staff_Choice;
-            cout << "Please enter your choice. \n";
-            cin >> Staff_Choice;
-             switch (Staff_Choice)
-            {
-            case 0:
-            {
-                changePassword(User);
-                if(User->Role == 1) SaveChangePassword(Staff_head);
-                else SaveChangePassword(Student_head);
-                break;
-            }
-            case 1:
-                AddNewYear(curYear);
-                break;
-            case 2:
-                CreateClass(curYear); // can update
-                break;
-            case 3:
-                AddNewStudent(Year_head);
-                break;
-            case 4:
-                Semester_head = AddNewSemester(curYear);
-                break;
-            case 5:
-                AddCourse(Semester_head);
-                break;
-            case 6:
-                InputCSV_Course(Semester_head->CourseList);
-                break;
-            case 7:
-                UpdCourseInf(Semester_head->CourseList);
-                break;
-            case 8:
-                AddStudentToCourse(Semester_head->CourseList);
-                break;
-            case 9:
-                RmStudentFrCourse(Semester_head->CourseList);
-                break;
-            case 10:
-                DeleteCourse(Semester_head->CourseList);
-                break;
-            case 11:
-                viewClass(Year_head,curYear);
-                break;
-            case 12:
-                viewStudentClass(Year_head);
-                break;
-            case 13:
-                ViewListOfCourse(Semester_head->CourseList);
-                break;
-            case 14:
-                viewStudentsInCourse(Semester_head->CourseList);
-                break;
-            case 15:
-                ExportCourseToCSVFile(Year_head, "");
-
-//                 break;
-//             case 16:
-
+//             int Staff_Choice;
+//             cout << "Please enter your choice. \n";
+//             cin >> Staff_Choice;
+//              switch (Staff_Choice)
 //             {
-//                 string filepath = ImportScoreboardFromFileUser(Year_head);
-//                 SaveScorboardToFolderSchoolyear(filepath);
+//             case 0:
+//             {
+//                 changePassword(User);
+//                 if(User->Role == 1) SaveChangePassword(Staff_head);
+//                 else SaveChangePassword(Student_head);
 //                 break;
 //             }
+//             case 1:
+//                 AddNewYear(curYear);
+//                 break;
+//             case 2:
+//                 CreateClass(curYear); // can update
+//                 break;
+//             case 3:
+//                 AddNewStudent(Year_head);
+//                 break;
+//             case 4:
+//                 Semester_head = AddNewSemester(curYear);
+//                 break;
+//             case 5:
+//                 AddCourse(Semester_head);
+//                 break;
+//             case 6:
+//                 InputCSV_Course(Semester_head->CourseList);
+//                 break;
+//             case 7:
+//                 UpdCourseInf(Semester_head->CourseList);
+//                 break;
+//             case 8:
+//                 AddStudentToCourse(Semester_head->CourseList);
+//                 break;
+//             case 9:
+//                 RmStudentFrCourse(Semester_head->CourseList);
+//                 break;
+//             case 10:
+//                 DeleteCourse(Semester_head->CourseList);
+//                 break;
+//             case 11:
+//                 viewClass(Year_head,curYear);
+//                 break;
+//             case 12:
+//                 viewStudentClass(Year_head);
+//                 break;
+//             case 13:
+//                 ViewListOfCourse(Semester_head->CourseList);
+//                 break;
+//             case 14:
+//                 // viewStudentsInCourse(Semester_head->CourseList);
+//                 break;
+//             case 15:
+//                 ExportCourseToCSVFile(Year_head, "");
 
-            case 17:
-                UpdateStudentResult(Semester_head);
-                break;
-            case 18:
-                viewClassScoreboard(Year_head->ClassHead, Semester_head, curYear);
-                break;
-            case 19:
-                viewCourseScoreboard(Semester_head->CourseList);
-                break;
-            case 20:
-                PrintProfile_Staff(Staff_head, Staff_info_head);
-                break;
-            case 21:
-                quickInputClass1stYear(curYear);
-                break;
-            case 22:
+// //                 break;
+// //             case 16:
+
+// //             {
+// //                 string filepath = ImportScoreboardFromFileUser(Year_head);
+// //                 SaveScorboardToFolderSchoolyear(filepath);
+// //                 break;
+// //             }
+
+//             case 17:
+//                 UpdateStudentResult(Semester_head);
+//                 break;
+//             case 18:
+//                 // viewClassScoreboard(Year_head->ClassHead, Semester_head, curYear);
+//                 break;
+//             case 19:
+//                 viewCourseScoreboard(Semester_head->CourseList);
+//                 break;
+//             case 20:
+//                 PrintProfile_Staff(Staff_head, Staff_info_head);
+//                 break;
+//             case 21:
+//                 quickInputClass1stYear(curYear);
+//                 break;
+//             case 22:
                 
 //                 break;
 //             default:
