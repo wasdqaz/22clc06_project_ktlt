72c883b1b133aa10ef1f1c95bad705abd2a33f4f
DangTrieuKha <126672405+DangTrieuKha@users.noreply.github.com>
2023-05-14 09:14:00 +0700

    Update menu.cpp

--- menu.cpp
+++ menu.cpp
@@ -12,7 +12,7 @@
 //     Account *Student_head = nullptr;
 //     Account *Staff_head = nullptr;
 //     Account *User;
-//     Semester *curSmeter = nullptr, *Semester_head = nullptr;
+//     Semester *curSmeter = nullptr, *Semester_head;
 //     SchoolYear *Year_head = nullptr;
 //     Staff *Staff_info_head = nullptr;
 //     int Role;
@@ -78,7 +78,7 @@
 //             cout << "|18. View the scoreboard of a class.                                 |\n";
 //             cout << "|19. View the scoreboard of a course.                                |\n";
 //             cout << "|20. View profile.                                                   |\n";
-//             cout << "|21. Upload file student for class                                    |\n";
+//             cout << "|21. Upload file student in class                                    |\n";
 //             cout << "|22. Logout.                                                         |\n";
 //             cout << "------------------------------END-------------------------------------\n";
 
@@ -90,8 +90,6 @@
 //             case 0:
 //             {
 //                 changePassword(User);
-//                 if(User->Role == 1) SaveChangePassword(Staff_head);
-//                 else SaveChangePassword(Student_head);
 //                 break;
 //             }
 //             case 1:
@@ -125,7 +123,7 @@
 //                 DeleteCourse(Semester_head->CourseList);
 //                 break;
 //             case 11:
-//                 viewClass(Year_head,curYear);
+//                 // viewClass(Year_head);
 //                 break;
 //             case 12:
 //                 viewStudentClass(Year_head);
@@ -139,17 +137,17 @@
 //             case 15:
 //                 ExportCourseToCSVFile(Year_head, "");
 
-// //                 break;
-// //             case 16:
+//                 break;
+//             case 16:
 
-// //             {
-// //                 string filepath = ImportScoreboardFromFileUser(Year_head);
-// //                 SaveScorboardToFolderSchoolyear(filepath);
-// //                 break;
-// //             }
+//             {
+//                 string filepath = ImportScoreboardFromFileUser(Year_head);
+//                 SaveScorboardToFolderSchoolyear(filepath);
+//                 break;
+//             }
 
 //             case 17:
-//                 UpdateStudentResult(Semester_head);
+//                 // UpdateStudentResult(Semester_head->CourseList);
 //                 break;
 //             case 18:
 //                 // viewClassScoreboard(Year_head->ClassHead, Semester_head, curYear);
@@ -196,14 +194,8 @@
 //                 ViewScoreBoardStudent(Year_head, User);
 //                 break;
 //             case 3:
-//             {
 //                 changePassword(User);
-//                     if(User->Role == 1) SaveChangePassword(Staff_head);
-//                     else SaveChangePassword(Student_head);
 //                 break;
-
-//             }
-                
 //             case 4:
 //                 PrintProfile_Student(User, Year_head);
 //                 break;
@@ -218,7 +210,7 @@
 //                 break;
 //         }
 //     }
-//     deleteAllLists(Year_head, Student_head ,Staff_head, Staff_info_head);
+    
 //     //delete User;
 //     return 0;
 // }
