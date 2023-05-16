69707660749ee484d3fe3a0ac62d37f4fea010a8
wasdqaz <mrkhoiphamanh@gmail.com>
2023-05-16 10:32:15 +0700

    last update

--- menu2.cpp
+++ menu2.cpp
@@ -1,24 +1,24 @@
-#include "header.h"
-#include "functionOfDuy.h"
-#include "KhoiFunction.h"
+// #include "header.h"
+// #include "functionOfDuy.h"
+// #include "KhoiFunction.h"
 
-int main()
-{
-    while (true)
-    {
-        int role = LoginConsole();
-        if (role == 0)
-            return 0;
+// int main()
+// {
+//     while (true)
+//     {
+//         int role = LoginConsole();
+//         if (role == 0)
+//             return 0;
         
-        if (role == 1)
-        {
-            STAFF_MENU();
-        }
-        else
-        {
-            STUDENT_MENU();
-        }
-    }
+//         if (role == 1)
+//         {
+//             STAFF_MENU();
+//         }
+//         else
+//         {
+//             STUDENT_MENU();
+//         }
+//     }
 
-    return 0;
-}
+//     return 0;
+// }
